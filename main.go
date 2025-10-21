bd482fabb8b9d950377cb2e3d1213ec6e5f4c1cd
SadovnikNeVPrime <apexlegen.navi@gmail.com>
2025-10-21 18:05:30 +0300

    Update main.go
    json

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+)
+
+type User struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
+func (u *User) ageupdt(a int) {
+	u.Age = a
+}
 
 func main() {
-	fmt.Println("123")
+	jsonString := `{"name": "Саша", "age": 25, "email": "maria@mail.ru"}`
+	var sup User
+	err := json.Unmarshal([]byte(jsonString), &sup)
+	if err != nil {
+		log.Fatalln("Ошибка сериализации", err)
+	}
+	sup.ageupdt(43)
+	fmt.Println(sup)
 }
